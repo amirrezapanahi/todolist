cf444ef4f4e291f10d4b824b7919daf3b7ccebaf
Amir <asp20@st-andrews.ac.uk>
2023-01-10 23:16:24 +0000

    working todolist with gui

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,19 +1,87 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	"github.com/amirrezapanahi/todolist"
 )
 
 const (
-	todoFile = ".todos.json"
+	todoFile = "todos.json"
 )
 
 func main() {
-
-	add := flag.Bool("add", false, "add a new todo")
+	add := flag.Bool("add", false, "add a new task")
+	complete := flag.Int("complete", 0, "complete a task")
+	delete := flag.Int("delete", 0, "delete a task")
+	list := flag.Bool("list", false, "list out todos")
 
 	flag.Parse()
 
-	todos := &todo.Todos
+	todos := &todolist.Todos{Filename: todoFile}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		todos.Add(task)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *delete > 0:
+		err := todos.Delete(*delete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *list:
+		todos.Print()
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid command")
+		os.Exit(0)
+	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("Empty todo is not allowed")
+	}
+
+	return text, nil
 }
